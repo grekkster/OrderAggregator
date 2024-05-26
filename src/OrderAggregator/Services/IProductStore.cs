925f4b1a7e039d6f79cdd44f6ebb4b0c533fcf63
jura <jirapanek@gmail.com>
2024-05-26 18:25:27 +0200

    feat: add ProductStore

---
+++ src/OrderAggregator/Services/IProductStore.cs
@@ -0,0 +1,10 @@
+﻿using OrderAggregator.Models;
+
+namespace OrderAggregator.Services;
+
+public interface IProductStore
+{
+    void AddOrUpdateProduct(Order order);
+
+    Dictionary<int, ulong> GetAllOrders();
+}
---
+++ src/OrderAggregator/Services/ProductStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Concurrent;
+using OrderAggregator.Models;
+
+namespace OrderAggregator.Services;
+
+public class ProductStore : IProductStore
+{
+    private readonly ConcurrentDictionary<int, ulong> _orders = new();
+
+    public void AddOrUpdateProduct(Order order) => 
+        _orders.AddOrUpdate(order.ProductId, order.Quantity, (key, oldValue) => oldValue + order.Quantity);
+
+    public Dictionary<int, ulong> GetAllOrders() => _orders.ToDictionary();
+}
